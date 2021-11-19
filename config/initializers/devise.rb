6a424045a40baf1da6268d4fb21d3dcdb99c196f
MoretS <s.moret@ecole-ipssi.net>
2021-11-19 17:03:28 +0100

    Linting files

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -4,12 +4,11 @@
 # Many of these configuration options can be set straight in your model.
 
 Devise.setup do |config|
-    # ==> Configuration for :confirmable
-    # A period that the user is allowed to access the website even without
-    # confirming their account. For instance, if set to 2.days, the user will be
-    # able to access the website for two days without confirming their account,
-    # access will be blocked just in the third day. Default is 0.days, meaning
-    # the user cannot access the website without confirming their account.
-    config.allow_unconfirmed_access_for = 0.days
-  end
-  
+  # ==> Configuration for :confirmable
+  # A period that the user is allowed to access the website even without
+  # confirming their account. For instance, if set to 2.days, the user will be
+  # able to access the website for two days without confirming their account,
+  # access will be blocked just in the third day. Default is 0.days, meaning
+  # the user cannot access the website without confirming their account.
+  config.allow_unconfirmed_access_for = 0.days
+end
